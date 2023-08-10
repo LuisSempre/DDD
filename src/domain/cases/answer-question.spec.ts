a55608188e6420aae842d3d125111af930464101
LuisSempre <luis.sempre@viannasempre.com.br>
2023-08-10 01:30:19 -0300

    feat: Add repo

--- src/domain/cases/answer-question.spec.ts
+++ src/domain/cases/answer-question.spec.ts
@@ -2,10 +2,16 @@
 import { expect, test } from "vitest"
 import { AnswerQuestionUseCase } from "./answer-question"
 
-test('create an answer', () => {
+const fakeAnswerRepo: AnswersRepo = {
+	create: async (answer: Answer) => {
+		return;
+	}
+}
+
+test('create an answer', async () => {
   const answerQuestion = new AnswerQuestionUseCase()
 
-  const answer = answerQuestion.execute({
+  const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
     content: 'Nova resposta',
--- src/domain/cases/answer-question.ts
+++ src/domain/cases/answer-question.ts
@@ -1,5 +1,5 @@
-
 import { Answer } from "../entities/answer"
+import { AnswerRepo } from "../repo/answer-repo"
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -8,9 +8,15 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer(content)
-
+	constructor(
+		private answersRepo: AnswersRepo
+	) {}
+  async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({
+			content, 
+			authorId: instructorId, 
+			questionId
+		})
     return answer
   }
 }
